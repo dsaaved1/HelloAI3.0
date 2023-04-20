c6dd5931826ac0a834eead3e5c4936d8739cadc0
Diego <diego.saavedra.v102@gmail.com>
2023-04-20 11:12:02 -0500

    new register screen in sign up

--- src/screens/RegisterScreen/RegisterScreen.js
+++ src/screens/RegisterScreen/RegisterScreen.js
@@ -5,34 +5,42 @@ import CustomButton from '../../components/CustomButton';
 import SocialSignInButtons from '../../components/SocialSignInButtons';
 import {useNavigation} from '@react-navigation/core';
 import {useForm} from 'react-hook-form';
+import {useRoute} from '@react-navigation/native';
 import {Auth} from 'aws-amplify';
+import {colors} from '../../theme';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const EMAIL_REGEX =
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 const RegisterScreen = () => {
-  const {control, handleSubmit, watch} = useForm();
-  const pwd = watch('password');
-  const navigation = useNavigation();
+    const route = useRoute();
+    const {control, handleSubmit, watch} = useForm({
+      defaultValues: { email: route?.params?.email},
+    });
+    const pwd = watch('password');
+    const navigation = useNavigation();
 
-  const onRegisterPressed = async data => {
-    const {username, password, email, name} = data;
-    try {
-      await Auth.signUp({
-        username,
-        password,
-        attributes: {email, name, preferred_username: username},
-      });
+    const goBack = () => {
+        navigation.goBack();
+      };
 
-      navigation.navigate('ConfirmEmail', {username});
-    } catch (e) {
-      Alert.alert('Oops', e.message);
-    }
-  };
+    const onRegisterPressed = async data => {
+        const {password, name, username, email} = data;
+       
+        try {
 
-  const onSignInPress = () => {
-    navigation.navigate('SignIn');
-  };
+            await Auth.signUp({
+                username,
+                password,
+                attributes: {email, name, preferred_username: username},
+              });
+        
+            navigation.navigate('ConfirmEmail', {username});
+        } catch (e) {
+          Alert.alert('Oops', e.message);
+        }
+      };
+  
 
   const onTermsOfUsePressed = () => {
     console.warn('onTermsOfUsePressed');
@@ -43,32 +51,24 @@ const RegisterScreen = () => {
   };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <SafeAreaView>
+    
       <View style={styles.root}>
-        <Text style={styles.title}>HelloAI</Text>
-
-        <CustomInput
-          name="name"
-          control={control}
-          autoCapitalize="none"
-          placeholder="Name"
-          rules={{
-            required: 'Name is required',
-            minLength: {
-              value: 3,
-              message: 'Name should be at least 3 characters long',
-            },
-            maxLength: {
-              value: 24,
-              message: 'Name should be max 24 characters long',
-            },
-          }}
+      <Ionicons
+          name="arrow-back"
+          size={24}
+          color={colors.dark.secondaryLight}
+          style={styles.backIcon}
+          onPress={goBack}
         />
+      <Text style={styles.title}>Register</Text>
+       
 
-        <CustomInput
+      <Text style={styles.subTitle}>Pick a username</Text>
+      <CustomInput
           name="username"
           control={control}
-          placeholder="Username"
+          placeholder="Username (e.g. johndoe123)"
           rules={{
             required: 'Username is required',
             minLength: {
@@ -81,16 +81,30 @@ const RegisterScreen = () => {
             },
           }}
         />
+        <Text style={styles.info}>You won't be able to change this later!</Text>
+
+        <Text style={styles.subTitle}>Pick a name</Text>
         <CustomInput
-          name="email"
-          autoCapitalize="none"
+          name="name"
           control={control}
-          placeholder="Email"
+          autoCapitalize="none"
+          placeholder="Display Name (e.g. John Doe)"
           rules={{
-            required: 'Email is required',
-            pattern: {value: EMAIL_REGEX, message: 'Email is invalid'},
+            required: 'Name is required',
+            minLength: {
+              value: 3,
+              message: 'Name should be at least 3 characters long',
+            },
+            maxLength: {
+              value: 24,
+              message: 'Name should be max 24 characters long',
+            },
           }}
         />
+        <Text style={{...styles.info}}>You can always change this later!</Text>
+
+       
+       
         <CustomInput
           name="password"
           autoCapitalize="none"
@@ -105,6 +119,7 @@ const RegisterScreen = () => {
             },
           }}
         />
+        <Text style={styles.info}>Passwords must be at least 8 characters long!</Text>
         <CustomInput
           name="password-repeat"
           autoCapitalize="none"
@@ -115,6 +130,7 @@ const RegisterScreen = () => {
             validate: value => value === pwd || 'Password do not match',
           }}
         />
+        <Text style={{...styles.info,marginBottom: 20}}>Passwords must be at least 8 characters long!</Text>
 
         <CustomButton
           text="Register"
@@ -132,37 +148,51 @@ const RegisterScreen = () => {
           </Text>
         </Text>
 
-        <SocialSignInButtons />
 
-        <CustomButton
-          text="Have an account? Sign in"
-          onPress={onSignInPress}
-          type="TERTIARY"
-        />
       </View>
-    </ScrollView>
+    
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
-    alignItems: 'center',
-    padding: 20,
-    marginTop: Platform.OS === 'android' ? 0 : 100,
-  },
-  title: {
-    fontSize: 56,
-    fontWeight: 'bold',
-    color: '#3777f0',
-    marginBottom: 15
-  },
-  text: {
-    color: 'gray',
-    marginVertical: 10,
-  },
-  link: {
-    color: '#FDB075',
-  },
+    root: {
+        padding: 20,
+       // marginTop: Platform.OS === 'android' ? 20 : 100,
+      },
+      backIcon: {
+        alignSelf: 'flex-start',
+        marginBottom: 10,
+      },
+      title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#3B71F3',
+        marginBottom: 25,
+        marginTop: -8,
+        alignSelf: 'center',
+      },
+      subTitle: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: colors.dark.secondaryLight,
+        alignSelf: 'flex-start',
+        //marginTop: 10,
+        marginBottom: 5,
+      },
+      info: {
+        color: 'gray',
+        alignSelf: 'flex-start',
+        marginBottom: 15,
+        fontSize: 10
+      },
+      text: {
+        color: 'gray',
+        marginVertical: 10,
+      },
+      link: {
+        color: '#FDB075',
+      },
 });
 
 export default RegisterScreen;
--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -11,20 +11,14 @@ const EMAIL_REGEX =
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 const SignUpScreen = () => {
-  const {control, handleSubmit, watch} = useForm();
-  const pwd = watch('password');
+  const {control, handleSubmit} = useForm();
   const navigation = useNavigation();
 
   const onRegisterPressed = async data => {
-    const {username, password, email, name} = data;
+    const {email} = data;
     try {
-      await Auth.signUp({
-        username,
-        password,
-        attributes: {email, name, preferred_username: username},
-      });
-
-      navigation.navigate('ConfirmEmail', {username});
+      
+      navigation.navigate('RegisterScreen', {email});
     } catch (e) {
       Alert.alert('Oops', e.message);
     }
@@ -47,40 +41,8 @@ const SignUpScreen = () => {
       <View style={styles.root}>
         <Text style={styles.title}>HelloAI</Text>
 
-        <CustomInput
-          name="name"
-          control={control}
-          autoCapitalize="none"
-          placeholder="Name"
-          rules={{
-            required: 'Name is required',
-            minLength: {
-              value: 3,
-              message: 'Name should be at least 3 characters long',
-            },
-            maxLength: {
-              value: 24,
-              message: 'Name should be max 24 characters long',
-            },
-          }}
-        />
 
-        <CustomInput
-          name="username"
-          control={control}
-          placeholder="Username"
-          rules={{
-            required: 'Username is required',
-            minLength: {
-              value: 3,
-              message: 'Username should be at least 3 characters long',
-            },
-            maxLength: {
-              value: 24,
-              message: 'Username should be max 24 characters long',
-            },
-          }}
-        />
+      
         <CustomInput
           name="email"
           autoCapitalize="none"
@@ -91,33 +53,10 @@ const SignUpScreen = () => {
             pattern: {value: EMAIL_REGEX, message: 'Email is invalid'},
           }}
         />
-        <CustomInput
-          name="password"
-          autoCapitalize="none"
-          control={control}
-          placeholder="Password"
-          secureTextEntry
-          rules={{
-            required: 'Password is required',
-            minLength: {
-              value: 8,
-              message: 'Password should be at least 8 characters long',
-            },
-          }}
-        />
-        <CustomInput
-          name="password-repeat"
-          autoCapitalize="none"
-          control={control}
-          placeholder="Repeat Password"
-          secureTextEntry
-          rules={{
-            validate: value => value === pwd || 'Password do not match',
-          }}
-        />
+       
 
         <CustomButton
-          text="Register"
+          text="Next"
           onPress={handleSubmit(onRegisterPressed)}
         />
 
@@ -132,7 +71,7 @@ const SignUpScreen = () => {
           </Text>
         </Text>
 
-        {/* <SocialSignInButtons /> */}
+        <SocialSignInButtons />
 
         <CustomButton
           text="Have an account? Sign in"
@@ -158,7 +97,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: 'gray',
-    marginVertical: 10,
+    marginVertical: 15,
   },
   link: {
     color: '#FDB075',
